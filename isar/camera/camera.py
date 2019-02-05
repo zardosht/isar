bcd9fd44bc05e317262742081f273e1d38e36b1c
Zardosht Hodaie <hodaie@in.tum.de>
2019-02-05 14:31:21 +0100

    - [unstable, not working] trying to implement the camera service using process instead of thread. Useless :( ... OpenCV, Python Multiprocessing, PyQt, and macOS is a mixture that does not work (for example, you cannot create the capture object after the process is forked)
    Former-commit-id: af64153aa4cfa34d2da41f19833081d6d1e3fed1

--- isar/camera/camera.py
+++ isar/camera/camera.py
@@ -1,6 +1,7 @@
 import logging
 import multiprocessing
 import threading
+import traceback
 from ctypes import c_bool
 from queue import Queue
 import cv2
@@ -18,47 +19,24 @@ class CameraService(Service):
     def __init__(self, service_name=None, cam_id=0):
         super().__init__(service_name)
         _queue_size = 5
-        self._queue = Queue(_queue_size)
+        self._queue = multiprocessing.Queue(_queue_size)
         self.cam_id = cam_id
         self._capture = None
-        self._open_capture()
+
+        # self._open_capture()
+
         self._do_capture = multiprocessing.Value(c_bool, False)
         self._stop_event = multiprocessing.Event()
-        self._capture_process: multiprocessing.Process = None
-
-    def _open_capture(self):
-        self._capture = cv2.VideoCapture(self.cam_id)
-        if not self._capture.isOpened():
-            raise Exception("Could not open camera {}".format(self.cam_id))
+        self._capture_process = None
 
     def start(self):
-        self._capture_process = multiprocessing.Process(target=self._start_capture)
+        # self._capture_process = multiprocessing.Process(target=self._start_capture, args=(self._capture, ))
+
+        self._capture_process = CameraCaptureProcess(self.cam_id, self._queue, self._do_capture, self._stop_event)
+
         self._capture_process.daemon = True
+
         self._capture_process.start()
-
-    def _start_capture(self):
-        """
-        Read OpenCV frames and put them in the Queue.
-        It blocks if the queue is full.
-        """
-        if not self._capture.isOpened():
-            self._open_capture()
-
-        frame_number = -1
-        while not self._stop_event.is_set():
-            if not self._do_capture.value:
-                continue
-
-            if self._queue.full():
-                continue
-
-            ret, frame = self._capture.read()
-            if ret:
-                frame_number += 1
-                camera_frame = CameraFrame(frame, frame_number)
-                self._queue.put(camera_frame)
-            else:
-                logger.error("Capture was unsuccessful.")
 
     def stop(self):
         """
@@ -67,7 +45,6 @@ class CameraService(Service):
         """
         self.stop_capture()
         self._stop_event.set()
-        self._capture.release()
         self._capture_process.terminate()
         self._capture_process.join()
 
@@ -91,6 +68,53 @@ class CameraService(Service):
 
         return camera_frame
 
+    def get_camera_capture_size(self):
+        return self.capture_process.get_camera_capture_size()
+
+
+class CameraCaptureProcess(multiprocessing.Process):
+    def __init__(self, cam_id, cam_queue, do_capture, stop_event):
+        super().__init__()
+        self._capture = None
+        self.cam_id = cam_id
+        self._queue = cam_queue
+        self.do_capture = do_capture
+        self._stop_event = stop_event
+
+    def _open_capture(self):
+        try:
+            self._capture = cv2.VideoCapture(self.cam_id)
+        except Exception as exp:
+            traceback.print_tb(exp.__traceback__)
+
+        if not self._capture.isOpened():
+            raise Exception("Could not open camera {}".format(self.cam_id))
+
+    def run(self):
+        """
+        Read OpenCV frames and put them in the Queue.
+        It blocks if the queue is full.
+        """
+        self._open_capture()
+        frame_number = -1
+        while not self._stop_event.is_set():
+            if not self._capture or not self._capture.isOpened():
+                logger.error("Capture is none or not open.")
+                break
+
+            if not self._do_capture.value:
+                continue
+
+            if self._queue.full():
+                continue
+
+            ret, frame = self._capture.read()
+            if ret:
+                frame_number += 1
+                camera_frame = CameraFrame(frame, frame_number)
+                self._queue.put(camera_frame)
+            else:
+                logger.error("Capture was unsuccessful.")
 
     def get_camera_capture_size(self):
         if self._capture:
@@ -99,6 +123,10 @@ class CameraService(Service):
             return width, height
         else:
             return None
+
+    def terminate(self):
+        self._capture.release()
+        super().terminate()
 
 
 class CameraFrame:
