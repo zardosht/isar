57624e3a9878dfdab3879a00014e9645b321b2ae
Zardosht Hodaie <hodaie@in.tum.de>
2019-05-09 15:01:25 +0200

    - added two classes for Sonia's handskill training
    Former-commit-id: a855e136345b730daec34c2c50faa6f110f04fb3

---
+++ isar/handskilllearning/handskillannotations.py
@@ -0,0 +1,12 @@
+from isar.scene.annotationmodel import Annotation
+
+
+class MovingImageAnnotation(Annotation):
+    def __init__(self):
+        pass
+
+
+class PathAnnotation(Annotation):
+    pass
+
+
