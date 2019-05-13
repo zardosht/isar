adea3d915ce8a8884a04c6fbaebb92a79197731d
Zardosht Hodaie <hodaie@in.tum.de>
2019-05-13 21:13:23 +0200

    - added Show/HideAnnotationAction - added body for other actions - [TODO] next implement ShowSceneAction
    Former-commit-id: 9079275bbd7aa5eaa051a9dd088b66f4bb7113bf

--- isar/events/actionsservice.py
+++ isar/events/actionsservice.py
@@ -31,6 +31,16 @@ class ActionsService(Service):
         toggle_help.annotation_names = ["help_text1", "help_text2"]
         defined_actions.append(toggle_help)
 
+        show_lenna = ShowAnnotationAction()
+        show_lenna.name = "Show Lenna"
+        show_lenna.annotation_names = ["lenna"]
+        defined_actions.append(show_lenna)
+
+        hide_lenna = HideAnnotationAction()
+        hide_lenna.name = "Hide Lenna"
+        hide_lenna.annotation_names = ["lenna"]
+        defined_actions.append(hide_lenna)
+
     def perform_action(self, action):
         if action is None:
             logger.warning("Action is None.")
@@ -68,13 +78,19 @@ class ToggleAnnotationVisibilityAction(Action):
         self.annotation_names = None
 
     def run(self):
+        annotations = self.find_annotation()
+        for annotation in annotations:
+            is_visible = annotation.show.get_value()
+            annotation.show.set_value(not is_visible)
+
+    def find_annotation(self):
         if self.annotation_names is None:
             logger.error("annotation_name is none!")
-            return
+            return []
 
         if self.annotations_model is None:
             logger.error("AnnotationsModel is none!")
-            return
+            return []
 
         annotations = []
         for annotation_name in self.annotation_names:
@@ -85,12 +101,116 @@ class ToggleAnnotationVisibilityAction(Action):
             else:
                 annotations.append(annotation)
 
+        return annotations
+
+
+class ShowAnnotationAction(ToggleAnnotationVisibilityAction):
+    def __init__(self):
+        super().__init__()
+
+    def run(self):
+        annotations = self.find_annotation()
         for annotation in annotations:
-            is_visible = annotation.show.get_value()
-            annotation.show.set_value(not is_visible)
+            annotation.show.set_value(True)
 
 
+class HideAnnotationAction(ToggleAnnotationVisibilityAction):
+    def __init__(self):
+        super().__init__()
 
+    def run(self):
+        annotations = self.find_annotation()
+        for annotation in annotations:
+            annotation.show.set_value(False)
+
+
+class ShowSceneAction(Action):
+    """
+    Must have a scene as its target
+    """
+    def __init__(self):
+        super().__init__()
+
+    def run(self):
+        pass
+
+
+class NextSceneAction(Action):
+    """
+    Next scene in scene navigation sequence
+    TODO: We need a scene service that keeps track of scene navigation.
+    """
+    def __init__(self):
+        super().__init__()
+
+    def run(self):
+        pass
+
+
+class PreviousSceneAction(Action):
+    """
+    Previous scene in scene navigation sequence
+    """
+    def __init__(self):
+        super().__init__()
+
+    def run(self):
+        pass
+
+
+class PlaySoundAction(Action):
+    """
+    Must have a sound annotation as its target.
+    """
+    def __init__(self):
+        super().__init__()
+
+    def run(self):
+        pass
+
+
+class PlayVideoAction(Action):
+    """
+    Must have a video annotation as its target.
+    """
+    def __init__(self):
+        super().__init__()
+
+    def run(self):
+        pass
+
+
+class StartTimerAction(Action):
+    """
+    Must have a timer annotation as its target.
+    """
+    def __init__(self):
+        super().__init__()
+
+    def run(self):
+        pass
+
+
+class PauseTimerAction(Action):
+    """
+    Must have a timer annotation as its target.
+    """
+    def __init__(self):
+        super().__init__()
+
+    def run(self):
+        pass
+
+
+class ResetTimerAction(Action):
+    """
+    Must have a timer annotation as its target.
+    """
+    def __init__(self):
+        super().__init__()
+
+    def run(self):
+        pass
 
 
 
