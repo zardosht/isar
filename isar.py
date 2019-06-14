dae7d714145a5ca3684e35cf382e7db003571378
Sonia Klärmann <sonia@klaermann.ro>
2019-06-14 14:35:36 +0200

    - deleted the comments
    Former-commit-id: 73b3a2b1b0462378915b81a2f29406c3a926e92c

--- isar.py
+++ isar.py
@@ -36,40 +36,34 @@ def main():
     # See: https://stackoverflow.com/questions/50168647/multiprocessing-causes-python-to-crash-and-gives-an-error-may-have-been-in-progr
     # os.environ["OBJC_DISABLE_INITIALIZE_FORK_SAFETY"] = "YES"
 
-    # configure_logging()
-    # logger = logging.getLogger("isar")
+    configure_logging()
+    logger = logging.getLogger("isar")
 
     app = QtWidgets.QApplication(sys.argv)
 
-    # scene_defintion = input("Scene Definition (y/n)? ")
+    scene_defintion = input("Scene Definition (y/n)? ")
 
-    # all_services_initialized = servicemanager.start_services()
-    # if not all_services_initialized:
-    #     logger.error("Could not intialize services. Return.")
-    #     return
+    all_services_initialized = servicemanager.start_services()
+    if not all_services_initialized:
+        logger.error("Could not intialize services. Return.")
+        return
 
-    # if scene_defintion == "y":
-    #     isar.application_mode = ApplicationMode.AUTHORING
-    #     scene_def_window = SceneDefinitionWindow()
-    #     scene_def_window.show()
-    #     app.exec()
-    #
-    # elif scene_defintion == "n":
-    #     isar.application_mode = ApplicationMode.EXECUTION
-    #     domain_learning_window = DomainLearningWindow(screen_id=2)
-    #     domain_learning_window.move(100, 100)
-    #     domain_learning_window.show()
-    #     app.exec()
+    if scene_defintion == "y":
+        isar.application_mode = ApplicationMode.AUTHORING
+        scene_def_window = SceneDefinitionWindow()
+        scene_def_window.show()
+        app.exec()
 
-    # elif scene_defintion == "e":
-    isar.application_mode = ApplicationMode.EXECUTION
-    domain_learning_window = HandSkillExercisePathUI()
-    domain_learning_window.move(100, 100)
-    domain_learning_window.show()
-    app.exec()
+    elif scene_defintion == "n":
+        isar.application_mode = ApplicationMode.EXECUTION
+        domain_learning_window = DomainLearningWindow(screen_id=2)
+        domain_learning_window.move(100, 100)
+        domain_learning_window.show()
+        app.exec()
 
-    # time.sleep(0.5)
-    # servicemanager.stop_services()
+
+    time.sleep(0.5)
+    servicemanager.stop_services()
 
 
 if __name__ == "__main__":
