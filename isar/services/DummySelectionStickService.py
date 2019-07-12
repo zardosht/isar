9c7724a1f7d97ac2f4d38fb417d35244ffa96d47
Zardosht Hodaie <hodaie@in.tum.de>
2019-07-12 15:11:01 +0200

    - added a dummy service for debugging purposes
    Former-commit-id: 9f8971c3097bd022611bbfcc96f562bee892e280

--- isar/services/DummySelectionStickService.py
+++ isar/services/DummySelectionStickService.py
@@ -1,7 +1,7 @@
 from isar.services.service import Service
 
 
-class DummyObjectSelectionStickService(Service):
+class DummySelectionStickService(Service):
     def __init__(self, service_name):
         super().__init__(service_name)
 
--- isar/services/service.py
+++ isar/services/service.py
@@ -9,3 +9,9 @@ class Service:
 
     def stop(self):
         pass
+
+    def set_physical_objects_model(self, phys_obj_model):
+        pass
+
+    def set_annotations_model(self, annotations_model):
+        pass
